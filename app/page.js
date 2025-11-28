d397394d097f9366235ec81c109b4162881dcf67
Alexis PORIEL <alexis.poriel@neovity.fr>
2025-11-28 10:59:32 +0100

    Maps

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffectn, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { supabase } from "../lib/supabaseClient";
 
 // --- HELPERS UI ---
